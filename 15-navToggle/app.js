023a5179359792eed84ecc08e14330c2d96092dc
MKen212 <46496040+MKen212@users.noreply.github.com>
2021-09-04 18:24:44 +0200

    15 Nav Toggle

---
+++ 15-navToggle/app.js
@@ -0,0 +1,13 @@
+"use strict";
+// 15 - Nav Toggle JS
+
+// Get HTML Elements
+const menuToggleEl = document.getElementById("menuToggle");
+const activeElements = document.querySelectorAll(".active-element");
+
+// Add Click Event Listener to Toggle
+menuToggleEl.addEventListener("click", () => {
+  activeElements.forEach((element) => {
+    element.classList.toggle("active");
+  });
+});
